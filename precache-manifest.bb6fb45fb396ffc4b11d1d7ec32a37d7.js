3b4112cfd6d3621497da198fb1184ea86d354439
Anika <radianika@gmail.com>
2019-07-01 23:37:54 +0300

    Updates

---
+++ precache-manifest.bb6fb45fb396ffc4b11d1d7ec32a37d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037c29ba8b7f09d91914d0db73bc2398",
+    "url": "/React-static_post-list/index.html"
+  },
+  {
+    "revision": "164ea387f48d25e0dc69",
+    "url": "/React-static_post-list/static/css/main.0075f023.chunk.css"
+  },
+  {
+    "revision": "b2c48a8778e81c909972",
+    "url": "/React-static_post-list/static/js/2.0f690bc4.chunk.js"
+  },
+  {
+    "revision": "164ea387f48d25e0dc69",
+    "url": "/React-static_post-list/static/js/main.75af5838.chunk.js"
+  },
+  {
+    "revision": "bf16bbe34290c5604b22",
+    "url": "/React-static_post-list/static/js/runtime~main.33169d61.js"
+  }
+]);
